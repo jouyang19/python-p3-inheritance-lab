bde99456bee99716ae04aef8cbee4bd1c10d8e87
Guang <jouyang19@proton.me>
2024-06-06 16:43:47 -0400

    complete lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,9 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name, knowledge=""):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+    def learn(self, lesson):
+        if isinstance(lesson, str):
+            self._knowledge = self.knowledge.append(lesson)
--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,25 @@ from user import User
 import random
 
 class Teacher(User):
-
+    def __init__(self, first_name, last_name, knowledge="str is a data type in Python"):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
     def teach(self):
-        pass
+        index = random.randint(0, len(self.knowledge)-1)
+        lesson = self.knowledge[index]
+        print(lesson)
+        return lesson
+        
+t1 = Teacher("Sakib", "Rasul")
+t2 = Teacher("Ben", "Cavins")
+print("t1:", t1.teach())
+print("t2:", t2.teach())
